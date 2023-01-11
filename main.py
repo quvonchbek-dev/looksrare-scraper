2dffac5eed02f8b5c5b381971d34705202bcc77d
Quvonchbek <quvonchbek.dev@gmail.com>
2023-01-11 23:41:04 +0500

    final 2

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-from tools.scraper import scrape, write_as_json
+from tools.scraper import scrape, write_as_json, write_as_csv
 
+print("Hi, this is Looksrare collections scraper!")
+to_csv = input(
+    "The scraped data will write to data/data.json.\n\n"
+    "Do you want to write it to csv file? [y] or [n]\n"
+)
 soup = scrape()
-write_as_json(soup)
+if to_csv == "y":
+    write_as_csv(soup)
+else:
+    write_as_json(soup)
+print("THANK YOU!")
--- tools/scraper.py
+++ tools/scraper.py
@@ -1,36 +1,35 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.chrome.options import Options
 import json
 import time
-
+import pandas as pd
+from datetime import datetime
+from tqdm import tqdm
 
 base_url = "https://looksrare.org/"
 
 
 def scrape(all=False, save_html=False) -> BeautifulSoup:
     start_time = time.time()
-
-    capabilities = DesiredCapabilities.CHROME
-    capabilities["goog:loggingPrefs"] = {"performance": "ALL"}
-
-    driver = webdriver.Chrome(
-        "drivers/chromedriver.exe",
-        desired_capabilities=capabilities,
-    )
-
+    options = Options()
+    # options.add_argument("--headless")
+    driver = webdriver.Chrome(options=options, executable_path="drivers/")
+    print("[+] Browser is started.")
     driver.get(base_url + "collections")
     if all:
         driver.find_element(By.CLASS_NAME, "css-xc335k").click()
         time.sleep(1)
 
-    # clicking Load 'More button' 50 times
-    for i in range(50):
+    print("[+] Starting click 'Load More' button,  50 times")
+
+    for i in tqdm(list(range(50)), "Processing...", colour="#01ffbf"):
         try:
             button = driver.find_elements(By.TAG_NAME, "button")[-2]
             button.click()
-            print(f"{i+1} st click successful", end="\r")
             time.sleep(4)
         except:
             i -= 1
@@ -42,7 +41,7 @@ def scrape(all=False, save_html=False) -> BeautifulSoup:
             f.write(html)
 
     soup = BeautifulSoup(html, "lxml")
-    print(f"Data is scraped successful in {time.time() - start_time} seconds")
+    print(f"[+] Data is scraped successfully in {time.time() - start_time} seconds")
     return soup
 
 
@@ -63,20 +62,36 @@ def get_as_json(a: BeautifulSoup):
         floor = "-"
     data["floor"] = floor
     data["_24h_vol"] = css_0[2].div.div.div.text.strip()
-    data["total_vol"] = css_0[3].div.div.text.strip()
+    data["total_vol"] = css_0[3].div.div.text.strip().replace(",", ".")
     data["owners"] = css_0[4].div.text.strip()
-    data["items"] = css_0[5].div.text.strip()
+    data["items"] = css_0[5].div.text.strip().replace(",", ".")
     return data
 
 
-def write_as_json(soup: BeautifulSoup):
+def write_as_json(soup: BeautifulSoup) -> tuple:
     start_time = time.time()
     list_a = list(soup.find_all("a"))
-    data = list(map(get_as_json, list_a))
+    data = []
+    for a in tqdm(list_a, "Processing...", colour="#01ffbf"):
+        data.append(get_as_json(a))
     json.dump(data, open("data/data.json", "w"), indent=4)
-    print(f"Data is written to data.json in {time.time()-start_time} seconds")
+    print(f"[+] Data is written to data.json in {time.time()-start_time} seconds.")
+    return data
+
+
+def write_as_csv(soup: BeautifulSoup = None):
+    start_time = time.time()
+    if soup:
+        write_as_json(soup)
+    # data = json.load(open("data/data.json", "w"))
+    df = pd.read_json("data/data.json").drop(columns=["href"])
+    filename = f"data/{datetime.now().strftime('%Y-%m-%d %H%M')}.csv"
+    df.to_csv(filename, index=False)
+    print(f"[+] Data is written to {filename} in {time.time()-start_time} seconds.")
+    return True
 
 
 if __name__ == "__main__":
     # soup = BeautifulSoup(open("html\data.html", "r").read(), "html.parser")
-    scrape(all=False)
+    # write_as_csv()
+    scrape()
